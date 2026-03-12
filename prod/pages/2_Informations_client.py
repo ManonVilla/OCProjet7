d3ee553e52ee56d6e74ae1bcbe886b6e59d17a6c
ManonVilla <villamanon73@gmail.com>
2026-03-12 15:18:56 +0100

    correction page 2

--- prod/pages/2_Informations_client.py
+++ prod/pages/2_Informations_client.py
@@ -44,4 +44,5 @@ st.markdown("---")
 with st.expander("🔎 Voir toutes les variables brutes du client"):
     client_df = client_row.to_frame(name="Valeur").reset_index()
     client_df.columns = ["Variable", "Valeur"]
+    client_df = client_df.astype(str)
     st.dataframe(client_df, use_container_width=True, height=400)
